59432dca90f3a9ff5c3f8756ef38170dfbeddafb
rajib66k <rajib66k@gmail.com>
2025-06-28 20:47:58 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 let currentSong = new Audio();
-
 let songs;
 let currFolder;
 
@@ -9,28 +8,14 @@ function secondsToMinutesSeconds(seconds) {
     }
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = Math.floor(seconds % 60);
-    const formattedMinutes = String(minutes).padStart(2, '0');
-    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
-    return `${formattedMinutes}:${formattedSeconds}`;
+    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
 }
 
+// 🔧 Now fetch songs.json instead of reading folder
 async function getSongs(folder) {
     currFolder = folder;
-    let response = await fetch(`/Spotify/${currFolder}/`);
-    let text = await response.text();
-    let div = document.createElement("div")
-    div.innerHTML = text;
-    let as = div.getElementsByTagName("a")
-
-    songs = [];
-
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith("mp3")) {
-            songs.push(element.href.split(`/${currFolder}/`)[1])
-        }
-    }
-
+    let response = await fetch(`/Spotify/${currFolder}/songs.json`);
+    songs = await response.json();
     return songs;
 }
 
@@ -49,27 +34,27 @@ const playMusic = (track, pause = false) => {
 }
 
 function updatePlaylistUI() {
-    let songUl = document.querySelector(".playlist").getElementsByTagName("ul")[0];
+    let songUl = document.querySelector(".playlist ul");
     songUl.innerHTML = "";
     for (const song of songs) {
         songUl.innerHTML += `<li>
-                    <div class="play">
-                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path d="M5 20V4L19 12L5 20Z" stroke="#141B34" fill="#000" stroke-width="1.5"
-                                stroke-linejoin="round" />
-                        </svg>
-                    </div>
-                    <div class="info">
-                        <img class="current-cover" src="/Spotify/${currFolder}/cover.jpeg" alt="Cover Art">
-                        <div class="invisible">${song.replaceAll("%20", " ")}</div>
-                        <div class="visible">${song.replaceAll("%20", " ").replaceAll("Diljit Dosanjh", "").replaceAll(".mp3", "").replaceAll("(Official Video)", "").replaceAll("(Official Music Video)", "").replace("-", "").replaceAll("_", "").trim()}</div>
-                        <p>${song.replaceAll("%20", " ").split(" - ")[0]}</p>
-                    </div>
-                </li>`;
+            <div class="play">
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5 20V4L19 12L5 20Z" stroke="#141B34" fill="#000" stroke-width="1.5"
+                        stroke-linejoin="round" />
+                </svg>
+            </div>
+            <div class="info">
+                <img class="current-cover" src="/Spotify/${currFolder}/cover.jpeg" alt="Cover Art">
+                <div class="invisible">${song}</div>
+                <div class="visible">${song.replace(".mp3", "").replaceAll("_", " ").replaceAll("-", " ")}</div>
+                <p>${song.split(" - ")[0]}</p>
+            </div>
+        </li>`;
     }
 
-    Array.from(document.querySelector(".playlist").getElementsByTagName("li")).forEach(e => {
+    Array.from(document.querySelectorAll(".playlist li")).forEach(e => {
         e.addEventListener("click", () => {
             playMusic(e.querySelector(".invisible").innerHTML);
         });
@@ -77,35 +62,36 @@ function updatePlaylistUI() {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`/Spotify/songs/`);
-    let response = await a.text();
-    let div = document.createElement("div");
-    div.innerHTML = response;
-    let anchors = div.getElementsByTagName("a");
+    // 🔧 FIXED: Use index.json instead of broken folder fetch
+    let response = await fetch(`/Spotify/songs/index.json`);
+    let folders = await response.json();
+
     let bigalbum = document.querySelector(".bigalbum");
-    Array.from(anchors).forEach(async e => {
-        if (e.href.includes("/songs")) {
-            let folder = e.getAttribute('href').replace('/songs/', '').replace('/', '');
-            let a = await fetch(`/Spotify/songs/${folder}/info.json`);
-            let response = await a.json();
+
+    for (const folder of folders) {
+        try {
+            let res = await fetch(`/Spotify/songs/${folder}/info.json`);
+            let info = await res.json();
+
             bigalbum.innerHTML += `<div class="album" data-folder="${folder}">
-                        <div class="play">
-                            <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path d="M5 20V4L19 12L5 20Z" stroke="#141B34" fill="#000" stroke-width="1.5"
-                                    stroke-linejoin="round" />
-                            </svg>
-                        </div>
-                        <div class="info">
-                            <img src="/Spotify/songs/${folder}/cover.jpeg" alt="">
-                            <div class="invisible1"></div>
-                            <div class="visible">${response.description}</div>
-                            <p>${response.title}</p>
-                        </div>
-                    </div>`;
-            console.log(response);
+                <div class="play">
+                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M5 20V4L19 12L5 20Z" stroke="#141B34" fill="#000" stroke-width="1.5"
+                            stroke-linejoin="round" />
+                    </svg>
+                </div>
+                <div class="info">
+                    <img src="/Spotify/songs/${folder}/cover.jpeg" alt="">
+                    <div class="invisible1"></div>
+                    <div class="visible">${info.description}</div>
+                    <p>${info.title}</p>
+                </div>
+            </div>`;
+        } catch (err) {
+            console.error(`Error loading album ${folder}:`, err);
         }
-    });
+    }
 
     setTimeout(() => {
         Array.from(document.getElementsByClassName("album")).forEach(e => {
@@ -140,15 +126,15 @@ async function main() {
     });
 
     currentSong.addEventListener("timeupdate", () => {
-        document.querySelector(".songtime").innerHTML = `
-        ${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`;
+        document.querySelector(".songtime").innerHTML =
+            `${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`;
         document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
     });
 
     document.querySelector(".seekbar").addEventListener("click", e => {
         let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
         document.querySelector(".circle").style.left = percent + "%";
-        currentSong.currentTime = ((currentSong.duration) * percent) / 100;
+        currentSong.currentTime = (currentSong.duration * percent) / 100;
     });
 
     document.querySelector(".hamburger").addEventListener("click", () => {
@@ -161,7 +147,7 @@ async function main() {
 
     previous.addEventListener("click", () => {
         let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0]);
-        if ((index - 1) >= length) {
+        if ((index - 1) >= 0) {
             playMusic(songs[index - 1]);
         }
     });
@@ -171,16 +157,6 @@ async function main() {
         if ((index + 1) < songs.length) {
             playMusic(songs[index + 1]);
         }
-    });
-
-    Array.from(document.getElementsByClassName("album")).forEach(e => {
-        e.addEventListener("click", async item => {
-            songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`);
-            updatePlaylistUI();
-            if (songs.length > 0) {
-                playMusic(songs[0], true);
-            }
-        });
     });
 }
 
